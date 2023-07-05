6799ba32e6e2ea7d95aea42b514999238a136c03
Sanoop Koleri <sanoopkooleri@gmail.com>
2023-07-05 12:10:41 +0400

    Code uploaded

---
+++ CoreMicroserviceWithRabbitMq/Order.Microservice/Controllers/TicketController.cs
@@ -0,0 +1,74 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Ticketing.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+
+        private static readonly string[] Summaries = new[]
+       {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly IBus _bus;
+
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPost]
+        [Route("CreatePayment")]
+        public async Task<IActionResult> CreatePayment(Payment payment)
+        {
+            if (payment != null)
+            {
+                payment.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/paymentQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(payment);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
---
+++ CoreMicroserviceWithRabbitMq/OrderProcessor.Microservice/Consumers/PaymentConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Shared.Models;
+using System.Threading.Tasks;
+
+namespace TicketProcessor.Microservice.Consumers
+{
+    public class PaymentConsumer : IConsumer<Payment>
+    {
+        public async Task Consume(ConsumeContext<Payment> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
